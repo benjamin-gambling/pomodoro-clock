f45e15da4d4d942b7cfa8d32109abc95061d65a0
Benjamin Gambling <benjamin.gambling@gmail.com>
2020-08-07 11:58:30 -0700

    Updates

---
+++ precache-manifest.0668e7788b2d2702b3afb7d2d2bf4fb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c009caf8e54cec547edc25a83420d70",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "5311252117bacd6f456f",
+    "url": "/pomodoro-clock/static/css/main.51fb7d23.chunk.css"
+  },
+  {
+    "revision": "d1073c0accfb94c2b2b1",
+    "url": "/pomodoro-clock/static/js/2.2a0c7092.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.2a0c7092.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5311252117bacd6f456f",
+    "url": "/pomodoro-clock/static/js/main.f66fb01b.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  }
+]);
